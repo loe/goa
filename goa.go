a81b2c34b57d13a631b6d2b9fd4316ec31a9adc6
W. Andrew Loe III <andrew@andrewloe.com>
2015-01-23 11:36:48 -0800

    Let try having some flags to turn on logging and the port.

--- goa.go
+++ goa.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"github.com/elazarl/goproxy"
 	"log"
 	"net/http"
 )
 
 func main() {
+	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
+	addr := flag.String("addr", ":8080", "proxy listen address")
+	flag.Parse()
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
-	log.Fatal(http.ListenAndServe(":8080", proxy))
+	proxy.Verbose = *verbose
+	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
