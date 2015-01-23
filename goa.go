79cac70ccb2893956a7e1fabcda412cca24ab9f0
W. Andrew Loe III <andrew@andrewloe.com>
2015-01-23 11:42:04 -0800

    Simple request handler that logs the Authorization header.

--- goa.go
+++ goa.go
@@ -13,5 +13,13 @@ func main() {
 	flag.Parse()
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = *verbose
+
+	proxy.OnRequest().DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			auth := r.Header.Get("Authorization")
+			log.Printf("Authorization: %s", auth)
+			return r, nil
+		})
+
 	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
