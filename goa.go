c59b9d6efef8e5d43fe280382d77f7f1e42f3954
W. Andrew Loe III <andrew@andrewloe.com>
2015-01-26 13:07:53 -0800

    Lets use memcache to keep a running count of the number of requests used.

--- goa.go
+++ goa.go
@@ -3,28 +3,52 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/bradfitz/gomemcache/memcache"
 	"log"
 	"net/http"
 )
 
 type AuthProxy struct {
-	Director func(*http.Request)
+	Memcache *memcache.Client
 }
 
-func (p AuthProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p.Director(r)
-	fmt.Fprintln(w, "Great success!")
+func (proxy AuthProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	count, err := countAuthorization(r, proxy.Memcache)
+
+	if err == nil {
+		fmt.Fprintf(w, "Great success! => %d\n", count)
+	} else {
+		fmt.Fprintln(w, "Fail.")
+	}
 }
 
-func logAuthorization(req *http.Request) {
-	auth := req.Header.Get("Authorization")
-	log.Printf("Authorization: %s", auth)
+func countAuthorization(req *http.Request, m *memcache.Client) (count uint64, err error) {
+	auth, _ := extractAuthorization(req)
+
+	// attempt to increment by 1
+	count, err = m.Increment(auth, 1)
+
+	// if we get a miss perform an add, then incr again. Its fine if the add fails, it means another writer was first, the incr is still atomic.
+	if err == memcache.ErrCacheMiss {
+		// intentionally ignoring the error
+		m.Add(&memcache.Item{Key: auth, Value: []byte("0")})
+		count, err = m.Increment(auth, 1)
+	}
+
+	return count, err
+}
+
+func extractAuthorization(req *http.Request) (auth string, err error) {
+	auth = req.Header.Get("Authorization")
+
+	return auth, nil
 }
 
 func main() {
 	addr := flag.String("addr", ":8080", "proxy listen address")
 	flag.Parse()
 
-	proxy := AuthProxy{Director: logAuthorization}
+	mc := memcache.New("localhost:11211")
+	proxy := AuthProxy{Memcache: mc}
 	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
