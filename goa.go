c7b5254fb6cf0c7f07d76277012db80a70180937
W. Andrew Loe III <andrew@andrewloe.com>
2015-01-26 13:11:59 -0800

    Better names.

--- goa.go
+++ goa.go
@@ -8,12 +8,12 @@ import (
 	"net/http"
 )
 
-type AuthProxy struct {
+type tokenProxy struct {
 	Memcache *memcache.Client
 }
 
-func (proxy AuthProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	count, err := countAuthorization(r, proxy.Memcache)
+func (proxy tokenProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	count, err := countRequest(r, proxy.Memcache)
 
 	if err == nil {
 		fmt.Fprintf(w, "Great success! => %d\n", count)
@@ -22,26 +22,26 @@ func (proxy AuthProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func countAuthorization(req *http.Request, m *memcache.Client) (count uint64, err error) {
-	auth, _ := extractAuthorization(req)
+func countRequest(req *http.Request, m *memcache.Client) (count uint64, err error) {
+	token, _ := extractAuthorization(req)
 
 	// attempt to increment by 1
-	count, err = m.Increment(auth, 1)
+	count, err = m.Increment(token, 1)
 
 	// if we get a miss perform an add, then incr again. Its fine if the add fails, it means another writer was first, the incr is still atomic.
 	if err == memcache.ErrCacheMiss {
 		// intentionally ignoring the error
-		m.Add(&memcache.Item{Key: auth, Value: []byte("0")})
-		count, err = m.Increment(auth, 1)
+		m.Add(&memcache.Item{Key: token, Value: []byte("0")})
+		count, err = m.Increment(token, 1)
 	}
 
 	return count, err
 }
 
-func extractAuthorization(req *http.Request) (auth string, err error) {
-	auth = req.Header.Get("Authorization")
+func extractAuthorization(req *http.Request) (token string, err error) {
+	token = req.Header.Get("Authorization")
 
-	return auth, nil
+	return token, nil
 }
 
 func main() {
@@ -49,6 +49,6 @@ func main() {
 	flag.Parse()
 
 	mc := memcache.New("localhost:11211")
-	proxy := AuthProxy{Memcache: mc}
+	proxy := tokenProxy{Memcache: mc}
 	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
